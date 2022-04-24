878955e076c93238709b55571ad1d3b442e565d9
nyankyu <fukujinn@gmail.com>
2022-04-25 00:21:52 +0900

    ignore invalid input

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -8,15 +8,18 @@ fn main() {
     let secret_number = rand::thread_rng().gen_range(1..101);
 
     loop {
-        let mut guess = String::new();
-
         println!("Please input your guess. (1 - 100)");
+
+        let mut guess = String::new();
 
         io::stdin()
             .read_line(&mut guess)
             .expect("Failed to read line");
 
-        let guess: u32 = guess.trim().parse().expect("Please type a number!");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
 
         match guess.cmp(&secret_number) {
             Ordering::Less => println!("Too small!"),
