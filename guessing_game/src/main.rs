18807a48c084fa0fb41c24208fd5909f88a39070
nyankyu <fukujinn@gmail.com>
2022-04-24 22:51:37 +0900

    add  the function to compare with input value

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,14 +1,13 @@
 use std::io;
 use rand::Rng;
+use std::cmp::Ordering;
 
 fn main() {
     println!("Guess the number!");
 
     let secret_number = rand::thread_rng().gen_range(1..101);
 
-    println!("The secret number is: {}", secret_number);
-
-    println!("Please input your guess.");
+    println!("Please input your guess. (1 - 100)");
 
     let mut guess = String::new();
 
@@ -16,5 +15,14 @@ fn main() {
         .read_line(&mut guess)
         .expect("Failed to read line");
 
-    println!("You guessed: {}", guess);
+    let guess: u32 = guess.trim().parse()
+        .expect("Please type a number!");
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!"),
+    }
+
+    println!("The secret number is : {}", secret_number);
 }
