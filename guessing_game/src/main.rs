91e48a110969290899b22faeba1d83f9211a176f
nyankyu <fukujinn@gmail.com>
2022-04-24 23:16:02 +0900

    repeat challenge

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,28 +1,30 @@
-use std::io;
 use rand::Rng;
 use std::cmp::Ordering;
+use std::io;
 
 fn main() {
     println!("Guess the number!");
 
     let secret_number = rand::thread_rng().gen_range(1..101);
 
-    println!("Please input your guess. (1 - 100)");
+    loop {
+        let mut guess = String::new();
 
-    let mut guess = String::new();
+        println!("Please input your guess. (1 - 100)");
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
 
-    let guess: u32 = guess.trim().parse()
-        .expect("Please type a number!");
+        let guess: u32 = guess.trim().parse().expect("Please type a number!");
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You win!"),
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
     }
-
-    println!("The secret number is : {}", secret_number);
 }
