5eaa4758100dc7009d4e0f8b38bddbf618ce3267
nyankyu <fukujinn@gmail.com>
2022-04-25 19:15:17 +0900

    use integer type

--- the_rust_programming_language/data_type/src/main.rs
+++ the_rust_programming_language/data_type/src/main.rs
@@ -1,6 +1,11 @@
 fn main() {
     let integer_i8: i8 = -42;
     let integer_i64: i64 = 12345;
-    println!("integer: {}", integer_i8);
-    println!("integer: {}", integer_i64);
+    let integer_usize: usize = 0b11001101;
+    println!("integer i8: {}", integer_i8);
+    println!("integer i64: {}", integer_i64);
+    println!("integer usize: {}", integer_usize);
+
+    println!("hoge {}", -1234i32);
+
 }
