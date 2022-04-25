a034e398cab66d9a21a3b1c67cf4b49bb45aec85
nyankyu <fukujinn@gmail.com>
2022-04-25 16:31:46 +0900

    confirm shadowing

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -3,4 +3,14 @@ fn main() {
     println!("The value of x is: {}", x);
     x = 6;
     println!("The value of x is: {}", x);
+
+    let y = 7;
+    let y = y + 10;
+    let y = y * 50;
+    let y = y / 3;
+    println!("y is: {}", y);
+
+    let spaces = "     ";
+    let spaces = spaces.len();
+    println!("spaces: {}", spaces);
 }
