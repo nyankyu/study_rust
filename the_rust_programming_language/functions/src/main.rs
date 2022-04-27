d73a0b5be79a39a9c5df7b335a68de0bd6155eb9
nyankyu <fukujinn@gmail.com>
2022-04-27 09:45:21 +0900

    augument

--- the_rust_programming_language/functions/src/main.rs
+++ the_rust_programming_language/functions/src/main.rs
@@ -1,9 +1,9 @@
 fn main() {
     println!("Hello, world!");
 
-    another_function();
+    another_function(42, 0.0001);
 }
 
-fn another_function() {
-    println!("Another function.");
+fn another_function(x: i32, y: f32) {
+    println!("The value of x is: {}, y is: {}", x, y);
 }
