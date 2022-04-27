5b1359838b6bde6932c9a24636f9c402854cf92b
nyankyu <fukujinn@gmail.com>
2022-04-27 12:43:36 +0900

    finish function section

--- the_rust_programming_language/functions/src/main.rs
+++ the_rust_programming_language/functions/src/main.rs
@@ -3,7 +3,7 @@ fn main() {
 
     let x = {
         let y = 3;
-        y * 4
+        plus_one(y) * five()
     };
 
     another_function(x, 0.0001);
@@ -11,4 +11,12 @@ fn main() {
 
 fn another_function(x: i32, y: f32) {
     println!("The value of x is: {}, y is: {}", x, y);
+}
+
+fn five() -> i32 {
+    5
+}
+
+fn plus_one(x: i32) -> i32 {
+    x + 1
 }
