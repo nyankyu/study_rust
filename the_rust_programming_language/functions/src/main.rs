0bacfa821c00defbeb4f4fdf137e6cfde0f13c2e
nyankyu <fukujinn@gmail.com>
2022-04-27 09:59:20 +0900

    statement and expression

--- the_rust_programming_language/functions/src/main.rs
+++ the_rust_programming_language/functions/src/main.rs
@@ -1,7 +1,12 @@
 fn main() {
     println!("Hello, world!");
 
-    another_function(42, 0.0001);
+    let x = {
+        let y = 3;
+        y * 4
+    };
+
+    another_function(x, 0.0001);
 }
 
 fn another_function(x: i32, y: f32) {
