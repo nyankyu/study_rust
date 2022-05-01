c1088d6f40fff8b7a46556011e497f5f87cda271
nyankyu <fukujinn@gmail.com>
2022-05-01 18:25:45 +0900

    return values and scope

--- the_rust_programming_language/ownership/src/main.rs
+++ the_rust_programming_language/ownership/src/main.rs
@@ -20,6 +20,17 @@ fn main() {
 
     make_copy(x);
     println!("{}", x);
+
+    let s3 = give_ownership();
+    let s4 = String::from("hoge");
+    let s5 = takes_and_gives_back(s4);
+    println!("{}", s3);
+    //println!("{}", s4);
+    println!("{}", s5);
+
+    let s10 = String::from("abura katabura");
+    let (s20, len) = calculate_length(s10);
+    println!("The length of '{}' is {}.", s20, len);
 }
 
 fn takes_ownership(some_string: String) {
@@ -28,4 +39,17 @@ fn takes_ownership(some_string: String) {
 
 fn make_copy(some_integer: i32) {
     println!("{}", some_integer);
+}
+
+fn give_ownership() -> String {
+    String::from("World!")
+}
+
+fn takes_and_gives_back(a_string: String) -> String {
+    a_string
+}
+
+fn calculate_length(s: String) -> (String, usize) {
+    let len = s.len();
+    (s, len)
 }
