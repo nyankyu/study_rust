b50e9b593260b0e22b0a2066e41d1f61f54d0b1e
nyankyu <fukujinn@gmail.com>
2022-05-01 18:01:19 +0900

    clone(), copy trait

--- the_rust_programming_language/ownership/src/main.rs
+++ the_rust_programming_language/ownership/src/main.rs
@@ -5,4 +5,13 @@ fn main() {
 
     println!("{}", s);
     println!("{}", ss);
+
+    let x = 5;
+    let y = x;
+    println!("x: {}, y: {}", x, y);
+
+    let s1 = String::from("Hello");
+    let s2 = s1.clone();
+    println!("s1: [{}]", s1);
+    println!("s2: [{}]", s2);
 }
