4cab66b7bffef70374fe58b7158d02a02577ca5e
nyankyu <fukujinn@gmail.com>
2022-05-02 22:51:42 +0900

    mutable references

--- the_rust_programming_language/references_and_borrowing/src/main.rs
+++ the_rust_programming_language/references_and_borrowing/src/main.rs
@@ -1,10 +1,19 @@
 fn main() {
-    let s1 = String::from("hello");
-    let len = calculate_length(&s1);
+    let mut s1 = String::from("hello");
+    let mut s2 = String::from("hello");
+    //let len = calculate_length(&mut s1, &mut s1);
+    let len = calculate_length(&mut s1, &mut s2);
     println!("The length of '{}' is {}.", s1, len);
+
+    let mut a = String::from("hoge");
+    let r1 = &a;
+    let r2 = &a;
+    let r3 = &mut a;
+    //println!("{}", r1);
 }
 
-fn calculate_length(s: &String) -> usize {
-    //s.push_str(", world!");
-    s.len()
+fn calculate_length(s1: &mut String, s2: &mut String) -> usize {
+    s1.push_str(", world!");
+    s2.push_str(", world!");
+    s1.len() + s2.len()
 }
