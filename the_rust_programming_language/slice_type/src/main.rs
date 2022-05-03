3c68261cfdfa0b3e93a0e15890557621c8f150e6
nyankyu <fukujinn@gmail.com>
2022-05-03 21:04:04 +0900

    string slices

--- the_rust_programming_language/slice_type/src/main.rs
+++ the_rust_programming_language/slice_type/src/main.rs
@@ -1,16 +1,18 @@
 fn main() {
     let s = String::from("Hello, world!");
-    println!("{}: index of first word is {}", s, first_word(&s));
+    let word = first_word(&s);
+    //s.clear();
+    println!("first word is [{}]", word);
 }
 
-fn first_word(s: &String) -> usize {
+fn first_word(s: &String) -> &str {
     let bytes = s.as_bytes();
 
     for (i, &item) in bytes.iter().enumerate() {
         if item == b' ' {
-            return i;
+            return &s[..i];
         }
     }
 
-    s.len()
+    &s[..]
 }
