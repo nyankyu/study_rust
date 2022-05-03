e666ad57aa4d935755564ea78d74442c16437e48
nyankyu <fukujinn@gmail.com>
2022-05-03 21:51:15 +0900

    other slices

--- the_rust_programming_language/slice_type/src/main.rs
+++ the_rust_programming_language/slice_type/src/main.rs
@@ -1,11 +1,17 @@
 fn main() {
     let s = String::from("Hello, world!");
-    let word = first_word(&s);
+    let word = first_word(&s[..]);
     //s.clear();
     println!("first word is [{}]", word);
+
+    let a = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+    let a_slice = slice_2_5(&a[..]);
+    for i in a_slice {
+        println!("{}", i);
+    }
 }
 
-fn first_word(s: &String) -> &str {
+fn first_word(s: &str) -> &str {
     let bytes = s.as_bytes();
 
     for (i, &item) in bytes.iter().enumerate() {
@@ -15,4 +21,8 @@ fn first_word(s: &String) -> &str {
     }
 
     &s[..]
+}
+
+fn slice_2_5(a: &[i32]) -> &[i32] {
+    &a[2..6]
 }
