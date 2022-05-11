fc41a06196505b38e5599bd590da1ea87d7230b5
nyankyu <fukujinn@gmail.com>
2022-05-12 00:08:23 +0900

    tuple structs

--- the_rust_programming_language/structs/src/main.rs
+++ the_rust_programming_language/structs/src/main.rs
@@ -1,17 +1,35 @@
 fn main() {
-    struct User {
-        username: String,
-        email: String,
-        sign_in_count: u64,
-        active: bool,
-    }
+    let user1 = build_user(String::from("mail address"), String::from("name name"));
+    println!("{}", user1.email);
 
-    let user1 = User {
+    let user2 = User {
         email: String::from("aaa@bbb.ccc"),
         username: String::from("hogename"),
-        sign_in_count: 123,
-        active: false,
+        ..user1
     };
+    println!("{}", user2.username);
 
-    println!("{}", user1.username);
+    let bg_color = Color(0, 23, 255);
+    let point_p = Point3D(0.0, 0.0, 23.5);
+    println!("{}", bg_color.2);
+    println!("{}", point_p.2);
+}
+
+struct User {
+    username: String,
+    email: String,
+    sign_in_count: u64,
+    active: bool,
+}
+
+struct Color(i32, i32, i32);
+struct Point3D(f32, f32, f32);
+
+fn build_user(email: String, username: String) -> User {
+    User {
+        email,
+        username,
+        active: true,
+        sign_in_count: 1,
+    }
 }
