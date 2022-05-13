afe5797432314ad6771e33eaee0968da70e11306
nyankyu <fukujinn@gmail.com>
2022-05-13 19:04:52 +0900

    Use different types within an enum

--- the_rust_programming_language/ip_address/src/main.rs
+++ the_rust_programming_language/ip_address/src/main.rs
@@ -1,29 +1,13 @@
 #[derive(Debug)]
-enum IpAddrKind {
-    V4,
-    V6,
-}
-
-#[derive(Debug)]
-struct IpAddr {
-    kind: IpAddrKind,
-    address: String,
+enum IpAddr {
+    V4(u8, u8,u8, u8),
+    V6(String),
 }
 
 fn main() {
-    let home = IpAddr {
-        kind: IpAddrKind::V4,
-        address: String::from("127.0.0.1"),
-    };
-    let loopback = IpAddr {
-        kind: IpAddrKind::V6,
-        address: String::from("::1"),
-    };
+    let home = IpAddr::V4(127, 0, 0, 1);
+    let loopback = IpAddr::V6(String::from("::1"));
 
-    println!("{:#?}", home);
-    println!("{:#?}", loopback);
-}
-
-fn print_ip_type(ip_type: &IpAddrKind) {
-    println!("type of IP is {:?}", ip_type);
+    println!("{:?}", home);
+    println!("{:?}", loopback);
 }
