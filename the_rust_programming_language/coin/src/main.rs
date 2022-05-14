0ae64b558f48ad6e4247f8ac5d2028bf061e419c
nyankyu <fukujinn@gmail.com>
2022-05-14 18:37:36 +0900

    Use Option at match

--- the_rust_programming_language/coin/src/main.rs
+++ the_rust_programming_language/coin/src/main.rs
@@ -24,7 +24,29 @@ fn value_in_cents(coin: &Coin) -> u32 {
     }
 }
 
+fn plus_one(x: Option<i32>) -> Option<i32> {
+    match x {
+        None => None,
+        Some(i) => Some(i + 1),
+    }
+}
+
 fn main() {
     let my_coin = Coin::Quarter(UsState::Alabama);
     println!("{}", value_in_cents(&my_coin));
+
+    let five = Some(5);
+    let six = plus_one(five);
+    let none = plus_one(None);
+    println!("{:?}", six);
+    println!("{:?}", none);
+
+    let number = 2;
+    match number {
+        0 => println!("{} is Favorite Number!", number),
+        3 => println!("{} is Favorite Number!", number),
+        5 => println!("{} is Favorite Number!", number),
+        7 => println!("{} is Favorite Number!", number),
+        _ => println!("{} is NOT Favorite Number!", number),
+    }
 }
