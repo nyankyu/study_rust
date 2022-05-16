614cad20e8b554560e476ec6e487e0f77a6ea0f4
nyankyu <fukujinn@gmail.com>
2022-05-16 12:52:18 +0900

    separating modules into different files

--- the_rust_programming_language/restaurant/src/lib.rs
+++ the_rust_programming_language/restaurant/src/lib.rs
@@ -1,50 +1,9 @@
-mod front_of_house {
-    pub mod hosting {
-        pub fn add_to_waitlist() {}
-        pub fn seat_at_table() {}
-    }
+mod front_of_house;
+mod back_of_house;
 
-    pub mod serving {
-        fn take_order() {}
-        pub fn serve_order() {}
-        fn take_payment() {}
-    }
-}
+pub use crate::front_of_house::hosting;
 
-mod back_of_house {
-    fn fix_incorrect_order() {
-        cook_order();
-        super::front_of_house::serving::serve_order();
-    }
-
-    fn cook_order() {}
-
-    pub struct Breakfast {
-        pub toast: String,
-        seasonal_fruit: String,
-    }
-
-    impl Breakfast {
-        pub fn summer(toast: &str) -> Breakfast {
-            Breakfast {
-                toast: String::from(toast),
-                seasonal_fruit: String::from("peaches"),
-            }
-        }
-    }
-
-    pub enum Appetizer {
-        Soup,
-        Salad,
-    }
-}
-
-use crate::front_of_house::hosting;
-use self::front_of_house::hosting::seat_at_table;
-
-use std::{cmp::Ordering, iter};
-use std::io::{self, Write};
-use std::collections::*;
+use crate::hosting::seat_at_table;
 
 pub fn eat_at_restaurant() {
     hosting::add_to_waitlist();
@@ -60,9 +19,3 @@ pub fn eat_at_restaurant() {
     let order1 = back_of_house::Appetizer::Soup;
     let order2 = back_of_house::Appetizer::Salad;
 }
-
-use std::fmt::Result;
-use std::io::Result as IoResult;
-
-fn function1() -> Result {}
-fn function2() -> IoResult {}
