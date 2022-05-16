44f7d22d2d9434318962b4df77bfda9c8f559b57
nyankyu <fukujinn@gmail.com>
2022-05-16 21:00:49 +0900

    read element of vector

---
+++ the_rust_programming_language/collections/src/main.rs
@@ -0,0 +1,13 @@
+fn main() {
+    let mut v = vec![0, 1, 2, 3];
+
+    let third: &i32 = &v[2];
+    //v.push(10);
+    //v[0] = 10;
+    println!("The third element is {}", third);
+
+    match v.get(2) {
+        Some(value) => println!("The third element is {}", value),
+        None => println!("There is no third element."),
+    }
+}
