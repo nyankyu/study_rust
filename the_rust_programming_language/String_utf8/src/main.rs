da085cca1c7bc605284e9b4cbd49baa812d51a2e
nyankyu <fukujinn@gmail.com>
2022-05-18 21:24:09 +0900

    updating a string

--- the_rust_programming_language/String_utf8/src/main.rs
+++ the_rust_programming_language/String_utf8/src/main.rs
@@ -1,7 +1,18 @@
 fn main() {
-    let s = "hoge".to_string();
-    let ss = String::from("🍻");
+    let mut s = "hoge".to_string();
+    s.push_str(" piyo");
+    s.pop();
+    s.push('!');
 
-    println!("{}", s);
+    let ss = s + &String::from("!??");
+
+    //println!("{}", s);
     println!("{}", ss);
+
+    let s1 = String::from("tic");
+    let s2 = String::from("tac");
+    let s3 = String::from("toe");
+    //let s4 = s1 + "-" + &s2 + "-" + &s3;
+    let s4 = format!("{}-{}-{}", s1, s2, s3);
+    println!("{}", s4);
 }
