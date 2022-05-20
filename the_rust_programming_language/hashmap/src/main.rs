a8d09bc96cf8aed0bb34ee05595a2a795bd7a586
nyankyu <fukujinn@gmail.com>
2022-05-21 00:38:12 +0900

    accessing value in a hash map

--- the_rust_programming_language/hashmap/src/main.rs
+++ the_rust_programming_language/hashmap/src/main.rs
@@ -12,4 +12,24 @@ fn main() {
 
     let scores2: HashMap<_, _> = teams.iter().zip(initial_scores.iter()).collect();
     println!("{:?}", scores2);
+
+    let field_name = String::from("Favorite color");
+    let field_value = String::from("Blue");
+    let mut map = HashMap::new();
+    map.insert(field_name, field_value);
+    //println!("{}, {}", field_name, field_value);
+    println!("{:?}", map);
+
+    let mut int_str_map: HashMap<i32, &String> = HashMap::new();
+    let s = String::from("hoge");
+    int_str_map.insert(10, &s);
+    println!("{}", s);
+    println!("{:?}", int_str_map);
+
+    let score = scores.get(&String::from("Blue"));
+    println!("{}", score.unwrap());
+
+    for (key, value) in &scores {
+        println!("{}: {}", key, value);
+    }
 }
