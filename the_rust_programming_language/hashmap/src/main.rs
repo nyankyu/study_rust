428ef816ecd510cd21d64603d57de51b8e142924
nyankyu <fukujinn@gmail.com>
2022-06-16 03:47:34 +0900

    calculate mean, median and mode

--- the_rust_programming_language/hashmap/src/main.rs
+++ the_rust_programming_language/hashmap/src/main.rs
@@ -1,6 +1,8 @@
 use std::collections::HashMap;
+use rand::{thread_rng, Rng};
 
 fn main() {
+    /*
     let mut scores = HashMap::new();
     scores.insert(String::from("Blue"), 10);
     scores.insert(String::from("Yellow"), 25);
@@ -47,4 +49,36 @@ fn main() {
         *count += 1;
     }
     println!("{:?}", char_map);
+
+    println!("=======================");
+    */
+    let mut list = [0i8; 64];
+    thread_rng().fill(&mut list);
+    //println!("{:?}", list);
+
+    let (mean, median, mode) = make_mean_median_mode(&list);
+    println!("mean:\t{}", mean);
+    println!("median:\t{}", median);
+    println!("mode:\t{}", mode);
+}
+
+fn make_mean_median_mode(list: &[i8]) -> (f32, i8, i8) {
+    let mut v = Vec::from(list);
+    let half_len = v.len() / 2;
+    v.sort();
+    println!("{:?}", v);
+
+    let mut sum: i32 = 0;
+    let mut h = HashMap::new();
+    for i in &v {
+        let count = h.entry(i).or_insert(0);
+        *count += 1;
+        sum += *i as i32;
+    }
+
+    (
+        sum as f32 / list.len() as f32,
+        *(v.get(half_len).unwrap()),
+        **(h.iter().max_by(|a, b| a.1.cmp(b.1)).unwrap().0)
+    )
 }
