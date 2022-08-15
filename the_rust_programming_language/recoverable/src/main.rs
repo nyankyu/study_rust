28d5501c794f2efe181792a169dfb7401aa94609
nyankyu <fukujinn@gmail.com>
2022-08-15 22:51:59 +0900

    use expect()

--- the_rust_programming_language/recoverable/src/main.rs
+++ the_rust_programming_language/recoverable/src/main.rs
@@ -1,19 +1,5 @@
 use std::{fs::File, io::ErrorKind};
 
 fn main() {
-    let f = File::open("hello.txt");
-    let f = match f {
-        Ok(file) => file,
-        Err(ref error) if error.kind() == ErrorKind::NotFound => {
-            match File::create("hello.txt") {
-                Ok(fc) => fc,
-                Err(e) => {
-                    panic!("Tried to create file but there was a problem: {:?}", e)
-                },
-            }
-        },
-        Err(error) => {
-            panic!("There was a problem opening the file: {:?}", error)
-        },
-    };
+    let f = File::open("hello.txt").expect("Failed to open hello.txt");
 }
