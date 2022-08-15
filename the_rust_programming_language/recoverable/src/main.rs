44c472f8105aa069db14a9e5f3b5e63db763111e
nyankyu <fukujinn@gmail.com>
2022-08-15 23:23:19 +0900

    more ergonomic way

--- the_rust_programming_language/recoverable/src/main.rs
+++ the_rust_programming_language/recoverable/src/main.rs
@@ -4,10 +4,9 @@ use std::io::Read;
 use std::fs::File;
 
 fn read_username_from_file() -> Result<String, io::Error> {
-    let mut f = File::open("userlist.txt")?;
-    let mut s = String::new();
-    f.read_to_string(&mut s)?;
-    Ok(s)
+    let mut username = String::new();
+    File::open("userlist.txt")?.read_to_string(&mut username)?;
+    Ok(username)
 }
 
 fn main() {
