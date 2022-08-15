0fb9f53ed94d124c3d73ce9281152eb6d56e3712
nyankyu <fukujinn@gmail.com>
2022-08-15 23:10:05 +0900

    propagating errors

--- the_rust_programming_language/recoverable/src/main.rs
+++ the_rust_programming_language/recoverable/src/main.rs
@@ -1,5 +1,24 @@
-use std::{fs::File, io::ErrorKind};
+use std::f32::consts::E;
+use std::io;
+use std::io::Read;
+use std::fs::File;
+
+fn read_username_from_file() -> Result<String, io::Error> {
+    let f = File::open("userlist.txt");
+
+    let mut f = match f {
+        Ok(file) => file,
+        Err(e) => return Err(e),
+    };
+
+    let mut s = String::new();
+    match f.read_to_string(&mut s) {
+        Ok(_) => Ok(s),
+        Err(e) => Err(e),
+    }
+}
 
 fn main() {
-    let f = File::open("hello.txt").expect("Failed to open hello.txt");
+    let username = read_username_from_file().expect("fialed read userlist file.");
+    print!("{}", username);
 }
