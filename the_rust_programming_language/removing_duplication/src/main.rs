3b1e8c9e3766b380df354898682a16544002007d
nyankyu <fukujinn@gmail.com>
2022-09-11 22:13:51 +0900

    Add function for removing duplication

--- the_rust_programming_language/removing_duplication/src/main.rs
+++ the_rust_programming_language/removing_duplication/src/main.rs
@@ -1,13 +1,19 @@
 fn main() {
     let number_list = vec![34, 50, 25, 100, 65];
     println!("list is {:?}", number_list);
+    println!("The largest number is {}", largest(&number_list));
 
-    let mut largest = number_list[0];
-    for number in number_list {
-        if number > largest {
-            largest = number;
+    let number_list = vec![34, 50, 25, -100, 65];
+    println!("list is {:?}", number_list);
+    println!("The largest number is {}", largest(&number_list));
+}
+
+fn largest(list: &[i32]) -> i32 {
+    let mut largest = list[0];
+    for &item in list.iter() {
+        if item > largest {
+            largest = item;
         }
     }
-
-    println!("The largest number is {}", largest);
+    largest
 }
