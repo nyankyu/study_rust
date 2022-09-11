1cb8a00c2bb4a9022df8893a317da1f7bc0653ef
nyankyu <fukujinn@gmail.com>
2022-09-11 22:53:47 +0900

    make generic enum

--- the_rust_programming_language/removing_duplication/src/main.rs
+++ the_rust_programming_language/removing_duplication/src/main.rs
@@ -1,3 +1,4 @@
+
 fn main() {
     let point = Point {x: 5, y: 10};
     println!("point is [x: {}, y: {}]", point.x, point.y);
@@ -5,9 +6,18 @@ fn main() {
     println!("point is [x: {}, y: {}]", point.x, point.y);
     let point = Point {x: 10, y: -4.5};
     println!("point is [x: {}, y: {}]", point.x, point.y);
+
+    let h = Hoge::AAA(1, 'a');
+    println!("hoge {:?}", h);
 }
 
 struct Point<T, U> {
     x: T,
     y: U,
+}
+
+#[derive(Debug)]
+enum Hoge<T, U> {
+    AAA(T, U),
+    BBB,
 }
